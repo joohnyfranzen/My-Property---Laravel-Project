a37dee18f83f945fa21624d10b29d2744a03d6cd
Jonathan <joohny_2014@hotmail.com>
2022-09-07 16:20:05 -0300

    Migrations

---
+++ database/migrations/2022_09_07_190254_create_table_real_state.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('real_state', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('title');
+            $table->string('description');
+            $table->text('content');
+            $table->float('price');
+            $table->integer('bathrooms');
+            $table->integer('bedrooms');
+            $table->integer('property_area');
+            $table->integer('total_property_area');
+            $table->string('slug');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('real_state');
+    }
+};
