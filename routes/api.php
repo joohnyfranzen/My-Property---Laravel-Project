aca9325cb8e0f2cd5d4981ec91be7df05e4e9b04
Jonathan <joohny_2014@hotmail.com>
2022-09-07 18:08:27 -0300

    RealState endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\RealStateController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->namespace('Api')->group(function(){
+    Route::prefix('real-states')->name('real_states')->group(function(){
+        Route::get('/', [RealStateController::class, 'index'])->name('index');
+    });
 });
