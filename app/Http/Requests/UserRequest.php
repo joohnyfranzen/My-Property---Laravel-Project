0bd174ef5355b1392218a871c3acc1f478d963a4
Jonathan <joohny_2014@hotmail.com>
2022-09-13 18:12:54 -0300

    Temporary fix on UserRequest

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -23,10 +23,11 @@ class UserRequest extends FormRequest
      */
     public function rules()
     {
+        // Temporary fix
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['string', 'min:8', ],
+            // 'name' => ['required', 'string', 'max:255'],
+            // 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            // 'password' => ['string', 'min:8', ],
         ];
     }
 }
