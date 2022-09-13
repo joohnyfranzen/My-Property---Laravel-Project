158c9fa9c55b1340a7b8afe9cad6adae660c3b31
Jonathan <joohny_2014@hotmail.com>
2022-09-13 09:52:10 -0300

    RealStateSearchController index

--- app/Http/Controllers/Api/RealStateSearchController.php
+++ app/Http/Controllers/Api/RealStateSearchController.php
@@ -3,20 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\RealState;
 use Illuminate\Http\Request;
 
 class RealStateSearchController extends Controller
 {
+    private $realState;
+
+    public function __construct(RealState $realState)
+    {
+        $this->realState = $realState;
+    }
 
     public function index()
     {
+        $realState = $this->realState->paginate(10);
 
+        return response()->json([
+            'data' => $realState
+        ], 200);
     }
 
 
     public function show($id)
     {
-        
+
     }
 
 
