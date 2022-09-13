3f798e3861be03b68f6e78648e3b7c4490c4979c
Jonathan <joohny_2014@hotmail.com>
2022-09-13 09:21:42 -0300

    Renaming migration

--- database/migrations/2022_09_13_122037_create_table_addresses.php
+++ database/migrations/2022_09_13_122037_create_table_addresses.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('adresses', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('adresses');
+        Schema::dropIfExists('addresses');
     }
 };
